6217657710d8510b9c89dccd8fa48868dc35cf68
Dmytro Davidenko <dima080885@gmail.com>
2022-10-15 15:13:17 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,24 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const createIndgridientList = (ingredients) => {
+  const nodesList = [];
+  ingredients.forEach((ingridient) => {
+    const element = document.createElement("li");
+    element.textContent = ingridient;
+    element.classList.add("item");
+    nodesList.push(element);
+  });
+  return nodesList;
+};
+const list = document.querySelector("#ingredients");
+
+const ingridientsList = createIndgridientList(ingredients);
+
+list.append(...ingridientsList);
