2f23df804653b84a98bc6579fd8e5c1d1c321629
Dmytro Davidenko <dima080885@gmail.com>
2022-10-16 10:34:07 +0300

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,26 @@
+// Get element
+const input = document.querySelector("#validation-input");
+
+// Add event listener
+input.addEventListener("blur", checkInputValue);
+
+// Callback to handle event 'blur' on input
+function checkInputValue(event) {
+  const inputValue = event.currentTarget.value;
+  const valueLength = event.currentTarget.dataset.length;
+  if (inputValue.length !== Number.parseInt(valueLength)) {
+    setInvalidClass(event.currentTarget);
+  } else {
+    setValidClass(event.currentTarget);
+  }
+}
+
+function setInvalidClass(node) {
+  node.classList.remove("valid");
+  node.classList.add("invalid");
+}
+
+function setValidClass(node) {
+  node.classList.remove("invalid");
+  node.classList.add("valid");
+}
