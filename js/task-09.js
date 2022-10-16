b7edc1abfbcf0942ec30b997ee20646e6eb54a5a
Dmytro Davidenko <dima080885@gmail.com>
2022-10-16 11:29:05 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,23 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// Get elements
+const body = document.querySelector("body");
+const display = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+
+// Set event listener
+button.addEventListener("click", changeBodyColor);
+
+// Callback function. Change body color
+function changeBodyColor(event) {
+  // Get new color
+  const newColor = getRandomHexColor();
+
+  // Change body color
+  body.style.backgroundColor = newColor;
+
+  // Output current body color
+  display.textContent = newColor;
+}
