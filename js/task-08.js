89bfabe86890e48653b4893a38adb697c07ffdff
Dmytro Davidenko <dima080885@gmail.com>
2022-10-16 11:20:39 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,35 @@
+// Get elements
+const form = document.querySelector(".login-form");
 
+// Set event listener
+form.addEventListener("submit", formProcessing);
+
+// Callback to handle submit form event
+function formProcessing(event) {
+  // Destructuring variable names out of form object
+  const {
+    elements: {
+      email: { name: emailName, value: emailValue },
+      password: { name: pwdName, value: pwdValue },
+    },
+  } = event.currentTarget;
+
+  // Prevent default action (page reloading)
+  event.preventDefault();
+
+  // Check that all form fields are filled out
+  if (emailValue === "" || pwdValue === "") {
+    // If not - display a message
+    alert("Please note that all fields in the form must be completed.");
+  } else {
+    // If true - create an object with the appropriate properties and values
+    const formValues = {
+      [emailName]: emailValue,
+      [pwdName]: pwdValue,
+    };
+    console.log(formValues);
+
+    // Clear form fields
+    event.currentTarget.reset;
+  }
+}
