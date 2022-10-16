eada7d29d12d88585c0ad5b4e5834a4f88e90931
Dmytro Davidenko <dima080885@gmail.com>
2022-10-16 12:39:14 +0300

    task04 default

--- js/task-04.js
+++ js/task-04.js
@@ -1,78 +1,22 @@
-class Counter {
-  #counterValue;
-  static #BASES = [2, 10, 16];
-  constructor(
-    {
-      rootSelector = "body",
-      startValue = 0,
-      step = 1,
-      typeOfBaseSelect = "select",
-    } = {
-      rootSelector: "body",
-      startValue: 0,
-      step: 1,
-      typeOfBaseSelect: "select",
-    }
-  ) {
-    this.typeOfBaseSelect = typeOfBaseSelect;
-    this.createBaseSelect();
-    this.step = step;
-    this.startValue = startValue;
-    this.rootSelector = document.querySelector(rootSelector);
-    this.display = this.rootSelector.querySelector("#value");
+// Create counter
+let counterValue = 0;
+// Get elements
+const display = document.querySelector("#value");
+const incBtn = document.querySelector('[data-action="increment"]');
+const decBtn = document.querySelector('[data-action="decrement"]');
 
-    this.makeBtns();
+// Add event listener
+incBtn.addEventListener("click", incrementValue);
+decBtn.addEventListener("click", decrementValue);
 
-    this.setEventListener(this.incrBtn, this.incrementValue.bind(this));
-    this.setEventListener(this.decrBtn, this.decrementValue.bind(this));
-
-    this.setStartValue();
-    // this.addBasesSelect();
-  }
-
-  createBaseSelect() {}
-
-  makeBtns() {
-    this.incrBtn = this.rootSelector.querySelector('[data-action="increment"]');
-    this.incrBtn.textContent = "+" + this.step;
-    this.decrBtn = this.rootSelector.querySelector('[data-action="decrement"]');
-    this.decrBtn.textContent = "-" + this.step;
-  }
-
-  incrementValue() {
-    this.#counterValue += this.step;
-    this.setDisplay();
-  }
-
-  decrementValue() {
-    this.#counterValue -= this.step;
-    this.setDisplay();
-  }
-
-  setEventListener(button, callback) {
-    button.addEventListener("click", callback);
-  }
-
-  setStartValue() {
-    this.#counterValue = this.startValue;
-    this.setDisplay();
-  }
-
-  setDisplay() {
-    this.display.textContent = this.#counterValue.toString(10).toUpperCase();
-  }
-
-  // addBasesSelect() {
-  //   const markup = ``
-  //   this.rootSelector.append(select);
-  // }
+// Callback function handle click on increment button
+function incrementValue() {
+  counterValue += 1;
+  display.textContent = counterValue;
 }
 
-const newCounter = new Counter({
-  rootSelector: "#counter",
-  startValue: 0,
-  step: 2,
-  typeOfBaseSelect: "select",
-});
-console.log("~ newCounter", newCounter);
-console.log(Object.getPrototypeOf(newCounter));
+// Callback function handle click on decrement button
+function decrementValue() {
+  counterValue -= 1;
+  display.textContent = counterValue;
+}
